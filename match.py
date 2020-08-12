4ee0b2609596a92f1847e91560e76dbf384533d8
ShristiGup <guptashristi25@gmail.com>
2020-08-12 18:25:09 +0530

    A new file named match.py added

---
+++ match.py
@@ -0,0 +1,6 @@
+import sqlite3
+Mydb = sqlite3.connect('FantasyCricket.db')
+cur = Mydb.cursor()
+sql="CREATE TABLE stats(player TEXT,matches INTEGER,runs INTEGER,'100s' INTEGER,'50s' INTEGER,value INTEGER,ctg TEXT);"
+cur.execute(sql)
+Mydb.close()
