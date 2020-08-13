afb87771fa83921fb077edea0a8999e23ee90476
ShristiGup <guptashristi25@gmail.com>
2020-08-13 16:44:25 +0530

    Parameters name changed and main function contents updated.

--- match.py
+++ match.py
@@ -6,5 +6,13 @@ cur.execute(sql)
 Mydb.close()
 
 def main():
-  pass
+  print("The program has begin its execution...")
+  print("Your data is checked.")
+def check_data(min_val,max_val):
+  if min_val<10:
+    print("The minimum value is:",a)
+  if max_val>40:
+    print("The maximum value is:",b)
+
 main()
+
