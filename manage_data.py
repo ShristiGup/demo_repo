0b50c849cf54c35fae648139d363528370fee095
ShristiGup <guptashristi25@gmail.com>
2020-08-13 23:59:23 +0530

    Add a abs_val function and sq_rt function. Also extra lines were removed.

--- manage_data.py
+++ manage_data.py
@@ -3,17 +3,25 @@ import math
 def main():
   print("The program has begin its execution...")
   print("Your data is checked.")
+
 def check_data(min_val,max_val):
   if min_val<10:
     print("The minimum value is:",min_val)
   if max_val>40:
     print("The maximum value is:",max_val)
+
 def check_executed():
   return check_data(7,90)
+
 def sq_rt(n):
   return math.sqrt(n)
+  
+def abs_val(n):
+  return abs(n)
 
 main()
 check_executed()
 print(sq_rt(64))
+print(abs_val(-95.78))
+
 
